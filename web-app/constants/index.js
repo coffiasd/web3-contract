df3198d1ad2b3ccc7b7a0439fe60b45574353917
coffiasd <759291502@qq.com>
2022-06-06 14:15:35 +0000

    add abi

--- web-app/constants/index.js
+++ web-app/constants/index.js
@@ -1,2 +1,29 @@
-export const abi = ""
-export const MY_CONTRACT_ADDRESS = "address of your builded contract"
+export const abi = [
+    {
+        "inputs": [],
+        "name": "getMood",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_mood",
+                "type": "string"
+            }
+        ],
+        "name": "setMood",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
+export const MY_CONTRACT_ADDRESS = "0xf313a9e74d7ED4cecD4Be694898638a3afF99747"
